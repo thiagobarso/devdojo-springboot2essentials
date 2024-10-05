57002fb9e69bc185db259030b041cf7f49ce1479
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-05 16:28:21 -0300

    Spring Boot 2 Essentials 13 - Spring Data JPA pt 01

--- src/main/java/academy/devdojo/springboot2/ApplicationStart.java
+++ src/main/java/academy/devdojo/springboot2/ApplicationStart.java
@@ -1,10 +1,7 @@
 package academy.devdojo.springboot2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 //@EnableAutoConfiguration
 //@ComponentScan
