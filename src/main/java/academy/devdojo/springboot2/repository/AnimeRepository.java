ce9282b9b73bb5efe16f85a626e27c5ea7b601f2
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-05 16:54:22 -0300

    Spring Boot 2 Essentials 14 - Spring Data JPA pt 02

--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -1,10 +1,9 @@
 package academy.devdojo.springboot2.repository;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AnimeRepository {
-
-    public List<Anime> listAll();
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
 }
