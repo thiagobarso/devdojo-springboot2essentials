aa353912af3e1fea9e8627078045ed354390364b
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-14 16:51:26 -0300

    Spring Boot 2 Essentials 43 - Spring Security pt 04 - Autenticação com usuário no banco de dados

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package academy.devdojo.springboot2.config;
 
+import academy.devdojo.springboot2.service.DevDojoUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,14 +10,21 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Log4j2
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    var xsrfCookie = postman.getResponseCookie("XSRF-TOKEN");
-//postman.setEnvironmentVariable("x-xsrf-token", xsrfCookie.value);
+    private final DevDojoUserDetailsService devDojoUserDetailsService;
+
+
+    /**
+     * Código do Postman
+     * var xsrfCookie = postman.getResponseCookie("XSRF-TOKEN");
+     * postman.setEnvironmentVariable("x-xsrf-token", xsrfCookie.value);
+     */
+
     /**
      * BasicAuthenticatorFilter
      * UsernamePasswordAuthenticationFilter
@@ -23,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      * DefaultLogoutGeneratingFilter
      * FilterSecurityInterceptor
      * Authentication -> Authorization
+     *
      * @param http
      * @throws Exception
      */
@@ -45,14 +55,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        log.info("Password encoded {}", passwordEncoder.encode("test"));
+        log.info("Password encoded {}", passwordEncoder.encode("academy"));
         auth.inMemoryAuthentication()
-                .withUser("william")
+                .withUser("william2")
                 .password(passwordEncoder.encode("academy"))
                 .roles("USER", "ADMIN")
                 .and()
-                .withUser("devdojo")
+                .withUser("devdojo2")
                 .password(passwordEncoder.encode("academy"))
                 .roles("USER");
+        auth.userDetailsService(devDojoUserDetailsService).passwordEncoder(passwordEncoder);
     }
 }
