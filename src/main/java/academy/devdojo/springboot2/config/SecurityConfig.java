201b4ebadf8884d576698c9569d237e09a5eaf05
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-16 15:40:31 -0300

    Spring Boot 2 Essentials 48 - Spring Boot Actuator

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/animes/admin/**").hasRole("ADMIN")
                 .antMatchers("/animes/**").hasRole("USER")
+                .antMatchers("/actuator/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
