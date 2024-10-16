d2b43110a3d19a39294fe238cf0e3b1f9b736987
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-16 11:24:48 -0300

    Spring Boot 2 Essentials 46 - Documentação com SpringDoc OpenAPI pt 01

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -4,8 +4,10 @@ import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import academy.devdojo.springboot2.service.AnimeService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -27,7 +29,7 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public ResponseEntity<Page<Anime>> list(Pageable pageable) {
+    public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
         return ResponseEntity.ok(animeService.listAll(pageable));
     }
 
