df4bb6441eb4080fe2ed8e1b51ac6cc96f156f33
Thiago Barbosa Soares <thiagobarso@gmail.com>
2024-10-21 14:37:39 -0300

    Spring Boot 2 Essentials 50 - Monitorando métricas com gráficos no Grafana

--- src/main/java/academy/devdojo/springboot2/ApplicationStart.java
+++ src/main/java/academy/devdojo/springboot2/ApplicationStart.java
@@ -1,7 +1,11 @@
 package academy.devdojo.springboot2;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 //@EnableAutoConfiguration
 //@ComponentScan
@@ -11,4 +15,10 @@ public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
     }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+   }
 }
